030b00871e7c897ab89117945ee5696c34779f3d
sebayang <ibrahim.sebayang@gmail.com>
2020-09-18 10:07:10 +0700

    Divisions And Department CRUD

---
+++ PencatatanSuhuPekerja/PencatatanSuhuPekerjaClient/Controllers/DivisionsController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PencatatanSuhuPekerjaAPI.Models;
+
+namespace PencatatanSuhuPekerjaClient.Controllers
+{
+    public class DivisionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        readonly HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:44379/api/")
+        };
+
+        public IActionResult LoadDivisions()
+        {
+            //if (HttpContext.Session.GetString("JWToken") != null)
+            //{
+                //client.DefaultRequestHeaders.Add("Authorization", HttpContext.Session.GetString("JWToken"));
+                IEnumerable<Division> divisions;
+                var restask = client.GetAsync("divisions");
+                restask.Wait();
+                var result = restask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<List<Division>>();
+                    readTask.Wait();
+                    divisions = readTask.Result;
+                }
+                else
+                {
+                    divisions = Enumerable.Empty<Division>();
+                    ModelState.AddModelError(string.Empty, "Error Load Departments");
+                }
+                return Json(divisions);
+            //}
+            //else
+            //{
+                return Redirect("/error");
+            //}
+
+        }
+        public IActionResult GetDivision(string id)
+        {
+            //client.DefaultRequestHeaders.Add("Authorization", HttpContext.Session.GetString("JWToken"));
+            Division division;
+            var restask = client.GetAsync("divisions/" + id);
+            restask.Wait();
+            var result = restask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsAsync<Division>();
+                readTask.Wait();
+                division = readTask.Result;
+            }
+            else
+            {
+                division = null;
+                ModelState.AddModelError(string.Empty, "Error Load Department");
+            }
+            return Json(division);
+        }
+
+        public IActionResult InsertOrUpdateDivision(string id, Division division)
+        {
+            try
+            {
+                //client.DefaultRequestHeaders.Add("Authorization", HttpContext.Session.GetString("JWToken"));
+                var json = JsonConvert.SerializeObject(division);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(json);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                if (division.Id == null)
+                {
+                    var result = client.PostAsync("divisions", byteContent).Result;
+                    return Ok(200);
+                }
+                else if (division.Id == id)
+                {
+                    var result = client.PutAsync("divisions/" + id, byteContent).Result;
+                    return Ok(200);
+                }
+                return BadRequest(404);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public IActionResult DeleteDivision(string id)
+        {
+            //client.DefaultRequestHeaders.Add("Authorization", HttpContext.Session.GetString("JWToken"));
+            var result = client.DeleteAsync("divisions/" + id).Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return Ok(200);
+            }
+            return BadRequest(500);
+        }
+    }
+}
---
+++ PencatatanSuhuPekerja/PencatatanSuhuPekerjaClient/Controllers/TemperaturesController.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using PencatatanSuhuPekerjaAPI.Models;
+
+namespace PencatatanSuhuPekerjaClient.Controllers
+{
+    public class TemperaturesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        readonly HttpClient client = new HttpClient
+        {
+            BaseAddress = new Uri("https://localhost:44379/api/")
+        };
+
+        public IActionResult LoadTemperatures()
+        {
+            //if (HttpContext.Session.GetString("JWToken") != null)
+            //{
+                //client.DefaultRequestHeaders.Add("Authorization", HttpContext.Session.GetString("JWToken"));
+                IEnumerable<Temperature> temperatures;
+                var restask = client.GetAsync("temperatures");
+                restask.Wait();
+                var result = restask.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsAsync<List<Temperature>>();
+                    readTask.Wait();
+                    temperatures = readTask.Result;
+                }
+                else
+                {
+                    temperatures = Enumerable.Empty<Temperature>();
+                    ModelState.AddModelError(string.Empty, "Error Load Departments");
+                }
+                return Json(temperatures);
+            //}
+            //else
+            //{
+                //return Redirect("/error");
+            //}
+
+        }
+        public IActionResult GetTemperatures(string id)
+        {
+            //client.DefaultRequestHeaders.Add("Authorization", HttpContext.Session.GetString("token"));
+            Temperature temperature;
+            var restask = client.GetAsync("temperatures/" + id);
+            restask.Wait();
+            var result = restask.Result;
+            if (result.IsSuccessStatusCode)
+            {
+                var readTask = result.Content.ReadAsAsync<Temperature>();
+                readTask.Wait();
+                temperature = readTask.Result;
+            }
+            else
+            {
+                temperature = null;
+                ModelState.AddModelError(string.Empty, "Error Load Department");
+            }
+            return Json(temperature);
+        }
+
+        public IActionResult InsertOrUpdateDivision(string id, Temperature temperature)
+        {
+            try
+            {
+                //client.DefaultRequestHeaders.Add("Authorization", HttpContext.Session.GetString("token"));
+                var json = JsonConvert.SerializeObject(temperature);
+                var buffer = System.Text.Encoding.UTF8.GetBytes(json);
+                var byteContent = new ByteArrayContent(buffer);
+                byteContent.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+
+                if (temperature.TemperatureId == null)
+                {
+                    var result = client.PostAsync("temperatures", byteContent).Result;
+                    return Ok(200);
+                }
+                else if (temperature.TemperatureId == id)
+                {
+                    var result = client.PutAsync("temperatures/" + id, byteContent).Result;
+                    return Ok(200);
+                }
+                return BadRequest(404);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public IActionResult DeleteTemperature(string id)
+        {
+            //client.DefaultRequestHeaders.Add("Authorization", HttpContext.Session.GetString("token"));
+            var result = client.DeleteAsync("temperatures/" + id).Result;
+            if (result.IsSuccessStatusCode)
+            {
+                return Ok(200);
+            }
+            return BadRequest(500);
+        }
+    }
+}
